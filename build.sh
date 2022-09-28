5ec35ae287f3e616ceeb1fd17cca15f27f1c5569
Carlos Eduardo Díaz Valbuena <carlos@zuluapp.io>
2022-09-27 21:56:58 -0500

    changes

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ export SCOPE=develop-read
 export APPLICATION=prometheus-go
 export APPLICATION_ID=1234
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=~/githubcli-archive-keyring.gpg \
+&& chmod go+r ~/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=~/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > ~/null \
 && apt update \
 && apt install gh -y
 
@@ -18,8 +18,8 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of
 # && sudo apt update \
 # && sudo apt install gh -y
 
-# git config --global user.name $GIT_USER
-# gh auth login --with-token $GIT_PWD
+git config --global user.name $GIT_USER
+gh auth login --with-token $GIT_PWD
 
 # whoami
 
