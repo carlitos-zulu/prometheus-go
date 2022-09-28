b5e0193e04b48944a699fb52480dd8438d97ece5
Carlos Eduardo Díaz Valbuena <carlos@zuluapp.io>
2022-09-27 22:34:27 -0500

    changes

--- build.sh
+++ build.sh
@@ -6,22 +6,6 @@ export SCOPE=develop-read
 export APPLICATION=prometheus-go
 export APPLICATION_ID=1234
 
-# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-# && sudo apt update \
-# && sudo apt install gh -y
-
-# git config --global user.name $GIT_USER
-# gh auth login --with-token $GIT_PWD
-
-# whoami
-
-# cp go.mod ~/
-# cp go.sum ~/
-# cp -R cmd ~/
-# cp -R internal ~/
-
 (
     echo "machine github.com"
     echo "    login $GIT_USER"
@@ -32,15 +16,6 @@ export APPLICATION_ID=1234
 ) > ~/.netrc
 
 chmod 600 ~/.netrc
-
-# cat ~/.netrc
-
-# ls -lha ~/.netrc
-
-# cd
-
-# pwd
-# ls -lha
 
 # install packages and dependencies
 go mod tidy
