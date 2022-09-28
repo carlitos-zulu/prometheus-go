c814c11158d9abac4f66850684ac456a86f0da48
Carlos Eduardo Díaz Valbuena <carlos@zuluapp.io>
2022-09-27 21:29:47 -0500

    changes

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ export SCOPE=develop-read
 export APPLICATION=prometheus-go
 export APPLICATION_ID=1234
 
+git config --global url.https://$GH_ACCESS_TOKEN@github.com/.insteadOf https://github.com/
+
 # curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 # && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 # && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
